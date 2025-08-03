3dd2fe0045a53e692c2e9524fe27da2ce5f83089
Searge <searge@duck.com>
2025-08-03 11:19:36 +0300

    fix(scala): Reformat according to warnings

--- src/scala/Ch01Intro.scala
+++ src/scala/Ch01Intro.scala
@@ -5,10 +5,10 @@
 object Ch01Intro:
 
   /** Calculate the score of a word by counting its characters imperatively.
-   *
-   * @param word The input word to score.
-   * @return The number of characters in the word.
-   */
+    *
+    * @param word The input word to score.
+    * @return The number of characters in the word.
+    */
   def calculateScore(word: String): Int =
     var score: Int = 0
     for _ <- word do
@@ -16,17 +16,17 @@ object Ch01Intro:
     score
 
   /** Calculate the score of a word declaratively using built-in length.
-   *
-   * @param word The input word to score.
-   * @return The number of characters in the word.
-   */
+    *
+    * @param word The input word to score.
+    * @return The number of characters in the word.
+    */
   def wordScore(word: String): Int = word.length
 
   /** Calculate the score of a word by counting non-'a' characters imperatively.
-   *
-   * @param word The input word to score.
-   * @return The number of characters in the word excluding 'a'.
-   */
+    *
+    * @param word The input word to score.
+    * @return The number of characters in the word excluding 'a'.
+    */
   def calculateScore2(word: String): Int =
     var score: Int = 0
     for char <- word do
@@ -35,29 +35,26 @@ object Ch01Intro:
     score
 
   /** Calculate the score of a word declaratively by subtracting 'a' count.
-   *
-   * @param word The input word to score.
-   * @return The number of characters in the word excluding 'a'.
-   */
-  def wordScore2(word: String): Int =
-    word.length - word.count(_ == 'a')
+    *
+    * @param word The input word to score.
+    * @return The number of characters in the word excluding 'a'.
+    */
+  def wordScore2(word: String): Int = word.length - word.count(_ == 'a')
 
   /** Remove all occurrences of a character from a string.
-   *
-   * @param word The input string.
-   * @param char The character to remove.
-   * @return A new string with all occurrences of char removed.
-   */
-  def stringWithoutChar(word: String, char: Char): String =
-    word.filter(_ != char)
+    *
+    * @param word The input string.
+    * @param char The character to remove.
+    * @return A new string with all occurrences of char removed.
+    */
+  def stringWithoutChar(word: String, char: Char): String = word.filter(_ != char)
 
   /** Calculate word score by removing 'a' characters and counting length.
-   *
-   * @param word The input word to score.
-   * @return The number of characters in the word excluding 'a'.
-   */
-  def wordScore3(word: String): Int =
-    stringWithoutChar(word, 'a').length
+    *
+    * @param word The input word to score.
+    * @return The number of characters in the word excluding 'a'.
+    */
+  def wordScore3(word: String): Int = stringWithoutChar(word, 'a').length
 
   // Main method to run the examples
   @main def runExamples(): Unit =
